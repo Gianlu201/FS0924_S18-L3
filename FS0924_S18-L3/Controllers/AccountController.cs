20710850fb43dfabc6777045a5a71839f9ed5a07
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-03-19 17:03:30 +0100

    AccountController added

---
+++ FS0924_S18-L3/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using FS0924_S18_L3.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FS0924_S18_L3.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            RoleManager<ApplicationRole> roleManager
+        )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ FS0924_S18-L3/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FS0924_S18_L3.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public required string Email { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+    }
+}
